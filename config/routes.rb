d35e8645866e409a15e729e533661177b5ca5bf5
huynhlv <huynhlevan1997@gmail.com>
2019-03-25 09:55:59 +0700

    Chapter 4: Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
+  root "static_pages#home"
+  get  "static_pages/home"
+  get  "static_pages/help"
+  get  "static_pages/about"
+  scope "(:locale)", locale: /en|vi/ do
+  end
 end
