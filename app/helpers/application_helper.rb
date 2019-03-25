9f824e96791ee771666767318a2f9dd21c487d1f
huynhlv <huynhlevan1997@gmail.com>
2019-03-25 16:46:54 +0700

    Chapter 5: Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,9 @@
 module ApplicationHelper
   def full_title page_title
-    base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.blank?
-      base_title
+      t(".base_title")
     else
-      page_title + " | " + base_title
+      page_title + " | " + t(".base_title")
     end
   end
 end
