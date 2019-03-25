727d418df4abb02a795040dd1471c6cfaceebd5e
huynhlv <huynhlevan1997@gmail.com>
2019-03-25 16:46:54 +0700

    Chapter 8: Basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       flash[:success] = t "controllers.users_controller.welcome_sample"
+      log_in @user
       redirect_to @user
     else
       render :new
