178153e1e3b0a7414458150e2771a20e017a2abf
huynhlv <huynhlevan1997@gmail.com>
2019-04-08 08:40:12 +0700

    Chapter 14: Following users

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,11 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   default_scope ->{order created_at: :desc}
-  scope :load_by_user_id, ->(id){where "user_id = ?", id}
+  scope :load_by_user_id, (lambda do |id|
+    following_ids = "SELECT followed_id FROM relationships
+      WHERE follower_id = :user_id"
+    (where "user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end)
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true,
